72fddc58867d9551d6b332772b7eb3420d5d54f7
Gabriel Robert <g.robert092@gmail.com>
2018-02-17 12:08:39 -0500

    Xamarin.Forms setup and Swank initialization

---
+++ tests/Swank.FormsPlugin.iOS.Tests/App.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+
+namespace Swank.FormsPlugin.iOS.Tests
+{
+    public class App : Xamarin.Forms.Application
+    {
+        public App()
+        {
+            MainPage = new NavigationPage(new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Padding = new Thickness(32, 32, 32, 32),
+                    Children =
+                    {
+                        new Label {Text = "Swank - iOS"}
+                    }
+                }
+            });
+        }
+    }
+}
