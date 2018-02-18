52292b5f856949b57a3cc62dca124dee4e59162e
Gabriel Robert <g.robert092@gmail.com>
2018-02-18 09:49:42 -0500

    ViewerImage is observable

--- src/Swank.FormsPlugin.Abstractions/ViewerImage.cs
+++ src/Swank.FormsPlugin.Abstractions/ViewerImage.cs
@@ -2,9 +2,22 @@
 
 namespace Swank.FormsPlugin.Abstractions
 {
-    public class ViewerImage
+    public class ViewerImage : ObservableObject
     {
-        public ImageSource Source { get; set; }
-        public bool Is360 { get; set; }
+        public ImageSource Source
+        {
+            get => _source;
+            set => SetProperty(ref _source, value);
+        }
+
+        public bool Is360
+        {
+            get => _is360;
+            set => SetProperty(ref _is360, value);
+        }
+
+        private bool _is360;
+
+        private ImageSource _source;
     }
 }
