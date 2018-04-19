b52423ad27bc645abc9a729d3c16b94dac8b2f45
Gabriel Robert <g.robert092@gmail.com>
2018-04-19 09:28:19 -0400

    Massive WIP

--- src/Shared/ViewerImageTemplate.cs
+++ src/Shared/ViewerImageTemplate.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
+using System.Reactive.Linq;
 using Xamarin.Forms;
+using Switch = Xamarin.Forms.Switch;
 
 namespace Plugin.Swank
 {
     public class ViewerImageTemplate : ContentView
     {
         private Viewer Viewer => Parent as Viewer;
-        public int width => 4000;
-        public int height => 2000;
+        private PanGestureRecognizer _pan = new PanGestureRecognizer();
+        public int width => 4096;
+        public int height => 2048;
 
         private readonly Image _image = new Image
         {
@@ -18,10 +22,11 @@ namespace Plugin.Swank
         private readonly Layout<View> _stackLayout = new StackLayout
         {
             HorizontalOptions = LayoutOptions.FillAndExpand,
-            Orientation = StackOrientation.Horizontal
+            Orientation = StackOrientation.Vertical
         };
 
-        private double x, y;
+        private double x, y = 0;
+        private int scale = 2;
 
         public ViewerImageTemplate()
         {
@@ -35,10 +40,10 @@ namespace Plugin.Swank
         {
             base.OnBindingContextChanged();
             var image = BindingContext as ViewerImage;
-            //if (image != null && image.Is360)
-            //{
-            //    Create360Controls(image);
-            //}
+            if (image != null && image.Is360)
+            {
+                Create360Controls(image);
+            }
         }
 
         private void Create360Controls(ViewerImage image)
@@ -49,21 +54,24 @@ namespace Plugin.Swank
             isBlockedSwitch.Toggled += IsBlockedSwitchOnToggled;
 
             // Add components to stacklayout
-            _stackLayout.Children.Add(isBlockedSwitch);
+            _stackLayout.Children.Insert(0, isBlockedSwitch);
         }
 
         private void OnDrag(object sender, PanUpdatedEventArgs e)
         {
-            Console.WriteLine($"OnDrag X:{e.TotalX} Y:{e.TotalY}");
-
             switch (e.StatusType)
             {
                 case GestureStatus.Running:
                     // Translate and ensure we don't pan beyond the wrapped user interface element bounds.
-                    Content.TranslationX =
-                        Math.Max(Math.Min(0, x + e.TotalX), -Math.Abs(Content.Width - width));
-                    Content.TranslationY =
-                        Math.Max(Math.Min(0, y + e.TotalY), -Math.Abs(Content.Height - height));
+                    var absWidth = -Math.Abs(Content.Width - (_image.Width * scale));
+                    var minX = Math.Min(300, x + e.TotalX);
+                    Content.TranslationX = Math.Max(minX, absWidth);
+                    Console.WriteLine($"absHeight: {absWidth} minY: {minX} translationY: {Content.TranslationX}");
+
+                    var absHeight = -Math.Abs(Content.Height - (_image.Height * scale));
+                    var minY = Math.Min(300, y + e.TotalY);
+                    Content.TranslationY = Math.Max(minY, absHeight);
+                    Console.WriteLine($"absHeight: {absHeight} minY: {minY} translationY: {Content.TranslationY}");
                     break;
 
                 case GestureStatus.Completed:
@@ -74,18 +82,86 @@ namespace Plugin.Swank
             }
         }
 
+        private IObservable<PanUpdatedEventArgs> PanUpdateAsObservable()
+        {
+            return Observable.FromEvent<EventHandler<PanUpdatedEventArgs>, PanUpdatedEventArgs>(
+                handler => (sender, e) => handler(e),
+                handler => _pan.PanUpdated += handler,
+                handler => _pan.PanUpdated -= handler
+            );
+        }
+
+        void SetPanObservable()
+        {
+            var sampleSeconds = 150; //サンプリングの間隔
+            var moveSeconds = 50; //移動アニメーションの時間
+
+            var panObservable = PanUpdateAsObservable();
+
+            var started = panObservable.Where(x => x.StatusType == GestureStatus.Started);
+            var completed = panObservable.Where(x => x.StatusType == GestureStatus.Completed);
+
+            started.Subscribe(x => Debug.WriteLine("Pan Started"));
+            completed.Subscribe(x => Debug.WriteLine("Pan Completed"));
+
+            var running = panObservable
+                .SkipWhile(x => x.StatusType != GestureStatus.Running)  //Running以外はスキップ
+                .TakeWhile(x => x.StatusType == GestureStatus.Running); //Runningの間は流し続ける
+
+            //Androidの場合一定時間でイベントをまとめて処理（カクカク対策）
+            if (Device.OS == TargetPlatform.Android)
+            {
+                running = running
+                    .Sample(TimeSpan.FromMilliseconds(sampleSeconds))   //一定時間ごとにサンプリング
+                    .StartWith(new PanUpdatedEventArgs(GestureStatus.Running, 0, 0, 0)); //最初だけ起点用に発行
+            }
+
+            //前の値が必要なので一つずらしたものとZipで合成
+            var drag = running.Zip(
+                        running.Skip(1),
+                        (p, n) => new { PreTotal = new Point(p.TotalX, p.TotalY), Total = new Point(n.TotalX, n.TotalY) }
+                    ).Repeat(); //繰り返し
+
+
+            var dragSub = drag.Subscribe(async p => {
+
+                //移動距離の計算
+                var distance = Device.OnPlatform(
+                    iOS: new Point(p.Total.X - p.PreTotal.X, p.Total.Y - p.PreTotal.Y),  //iOSは移動いてもTotalはリセットされないので差分を使用する
+                    Android: new Point(p.Total.X, p.Total.Y),    // Androidは移動後にTotalがリセットされるのでそのまま使う
+                    WinPhone: new Point());
+
+                //現在の位置を取得
+                var rectClip = AbsoluteLayout.GetLayoutBounds(_image);
+
+                //座標計算
+                var translationX = Math.Max(0, Math.Min(Content.Width - 4000, x + distance.X));
+                Console.WriteLine($"translationX: {translationX}");
+                Content.TranslationX = translationX;
+                var translationY = Math.Max(0, Math.Min(Content.Height - rectClip.Height, y + distance.Y));
+                Console.WriteLine($"translationY {translationY}");
+                Content.TranslationY = translationY;
+                x = translationX;
+                y = translationY;
+            });
+        }
+
         private void IsBlockedSwitchOnToggled(object sender, ToggledEventArgs toggledEventArgs)
         {
             Viewer.SetIsSwipeEnabled(toggledEventArgs.Value);
-            if (toggledEventArgs.Value)
+            if (toggledEventArgs.Value) 
             {
-                var panGesture = new PanGestureRecognizer();
-                panGesture.PanUpdated += OnDrag;
-                _stackLayout.GestureRecognizers.Add(panGesture);
+                // Create gesture
+                _stackLayout.GestureRecognizers.Add(_pan);
+                SetPanObservable();
+
+                // Image should scale properly
                 _image.Aspect = Aspect.Fill;
-                _stackLayout.Scale = 2;
-                Content.TranslationX = width / 2;
-                Content.TranslationY = height / 2;
+                _image.Scale = scale;
+
+                // Translation
+                //Content.TranslationX = width / 2;
+                //Content.TranslationY = height / 2;
             }
             else if (_stackLayout.GestureRecognizers.Any())
             {
--- tests/Swank.iOS.Tests/App.cs
+++ tests/Swank.iOS.Tests/App.cs
@@ -14,10 +14,10 @@ namespace Swank.iOS.Tests
             {
                 ItemsSource = new List<ViewerImage>
                 {
-                    new ViewerImage {Source = ImageSource.FromUri(new Uri("http://via.placeholder.com/1700x1275"))},
-                    new ViewerImage {Source = ImageSource.FromUri(new Uri("http://www.davidjmulder.com/wp-content/uploads/2014/07/Enq-360.jpg")), Is360 = true},
-                    new ViewerImage {Source = ImageSource.FromUri(new Uri("http://via.placeholder.com/400x300"))},
-                    new ViewerImage {Source = ImageSource.FromUri(new Uri("http://via.placeholder.com/900x100"))}
+                    //new ViewerImage {Source = ImageSource.FromUri(new Uri("http://via.placeholder.com/1700x1275"))},
+                    new ViewerImage {Source = ImageSource.FromUri(new Uri("https://farm1.static.flickr.com/853/40828093624_702a28cbf6_f.jpg")), Is360 = true},
+                    //new ViewerImage {Source = ImageSource.FromUri(new Uri("http://via.placeholder.com/400x300"))},
+                    //new ViewerImage {Source = ImageSource.FromUri(new Uri("http://via.placeholder.com/900x100"))}
                 }
             };
 
